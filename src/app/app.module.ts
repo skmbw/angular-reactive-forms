15410395b379b4bc353d725b6e7821f8ba5f058c
skmbw <tongku2008@126.com>
2018-04-23 17:43:40 +0800

    使用FormArray绑定 数据模型和表单模型

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,22 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import {HeroService} from './hero.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroListComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,18 +1,19 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
-import {Address, states} from '../data-model';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Address, Hero, states} from '../data-model';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
-export class HeroDetailComponent implements OnInit {
+export class HeroDetailComponent implements OnInit, OnChanges {
   // heroForm = new FormGroup({
   //   name: new FormControl()
   // });
   heroForm: FormGroup;
   states = states;
+  @Input() hero: Hero;
 
   constructor(private fb: FormBuilder) {
     this.createForm();
@@ -21,13 +22,40 @@ export class HeroDetailComponent implements OnInit {
   createForm() {
     this.heroForm = this.fb.group({
       name: ['', Validators.required], // <--- the FormControl called "name"
-      address: this.fb.group(new Address()), // 和数据模型保持接近
+      // address: this.fb.group(new Address()), // 和数据模型保持接近
+      secretLairs: this.fb.array([]), // <-- secretLairs as an empty FormArray
       power: '',
       sidekick: ''
     });
   }
 
+  setAddresses(addresses: Address[]) {
+    const addressFGs = addresses.map(address => this.fb.group(address));
+    const addressFormArray = this.fb.array(addressFGs);
+    this.heroForm.setControl('secretLairs', addressFormArray);
+  }
+
   ngOnInit() {
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    this.rebuildForm();
+  }
+
+  rebuildForm() {
+    // 绑定表单模型和数据模型
+    this.heroForm.reset({
+      name: this.hero.name
+      // address: this.hero.addresses[0] || new Address()
+    });
+    this.setAddresses(this.hero.addresses);
+  }
+
+  get secretLairs(): FormArray {
+    return this.heroForm.get('secretLairs') as FormArray;
+  }
+
+  addLair() {
+    this.secretLairs.push(this.fb.group(new Address()));
+  }
 }
